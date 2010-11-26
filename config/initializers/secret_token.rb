1ee80c0f0546956eb5945d79908096231e2a1662
adrek14 <adriansan@thedeathstar.(none)>
2010-11-26 20:45:40 +0100

    init & basic models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTask8::Application.config.secret_token = 'e3988be485d2782312991bede28bb0f397dc87e2935a5837429916190f6bd3ce8135e8b0af1aacad3c60f7cdab56b70e93828ed3268d9f056b920d72b579a2e8'
